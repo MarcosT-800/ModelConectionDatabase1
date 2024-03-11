024cef6fd646369a9ad93699010d89266f8d177b
Marcos Alves <82832426+MarcosT-800@users.noreply.github.com>
2024-03-11 10:50:12 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import routes from "./routes.js";
 import db from "./db.js";
+import cors from 'cors'
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
